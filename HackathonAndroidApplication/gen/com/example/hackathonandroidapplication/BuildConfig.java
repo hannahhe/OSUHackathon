fbaed6488da98795cd9aa1db0d55609bbfb1959c
Satvik11 <kethireddy.satvik@gmail.com>
2017-04-15 09:07:19 -0400

    Binaries added and right SDK used

---
+++ HackathonAndroidApplication/gen/com/example/hackathonandroidapplication/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.hackathonandroidapplication;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
